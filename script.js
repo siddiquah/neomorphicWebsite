fe0225b9cc212d97d01165ef1bc466a84f16aa3c
siddiquah <siddiquahanjum111@gmail.com>
2024-01-15 14:06:36 +0530

    home page edited and bg added

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
+AOS.init();
+
 var lastScrollTop = 0;
 var lineheight = 50;
 
-$(window).scroll(function(event){
+$(window).scroll(function (event) {
   var st = $(this).scrollTop();
-  var lineTop = $('h1').offset().top;  // Get the top position of the h1 element
-  
+  var lineTop = $("h1").offset().top; // Get the top position of the h1 element
+
   if (st > lastScrollTop) {
     // downscroll code
-    $('.backShapes span i').css({transform: "rotate(" + st/5 + "deg)" });
-    
+    $(".backShapes span i").css({ transform: "rotate(" + st / 5 + "deg)" });
+
     if (st > lineTop && lineheight < 550) {
       lineheight = lineheight + 10;
-      $('h1 span').css('height', lineheight + 'px');
+      $("h1 span").css("height", lineheight + "px");
     }
   } else {
     // upscroll code
-    $('.backShapes span i').css({transform: "rotate(" + st/5 + "deg)" });
+    $(".backShapes span i").css({ transform: "rotate(" + st / 5 + "deg)" });
 
     if (st > lineTop && lineheight > 50) {
       lineheight = lineheight - 10;
-      $('h1 span').css('height', lineheight + 'px');
+      $("h1 span").css("height", lineheight + "px");
     }
   }
   lastScrollTop = st;
-  
+
   checkAnimateIn(st);
 });
-
-
